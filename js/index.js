3d01435891715217e8a17a913728d092d0f428fb
Timur <timur-skvorcov-666@mail.ru>
2023-05-05 06:07:13 +0500

    message

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const headerChat = document.querySelector('.header__chat');
+const TmplheaderChat = document.querySelector('#tmpl');
+const TmplheaderChatInner = document.querySelector('.tpml__inner');
+
+const form = {
+  form: document.querySelector('form'),
+  headerAddMessageText: document.querySelector('.header__add-message-text'),
+  headerAddMessageAdd: document.querySelector('.header__add-message-add'),
+}
+
+function addMessage() {
+  const messageValue = form.headerAddMessageText.value
+  const spanContentText = TmplheaderChat.content.querySelector('.header__chat-message-content');
+
+  spanContentText.textContent = messageValue;
+  TmplheaderChatInner.append(TmplheaderChat.content.cloneNode(true))
+  headerChat.append(TmplheaderChatInner)
+}
+
+form.form.addEventListener('click', (event) => {
+  event.preventDefault()
+})
+
+form.headerAddMessageAdd.addEventListener('click', addMessage)
